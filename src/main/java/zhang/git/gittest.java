f4b33c245c03c7651787931f7eff9a5d80004cca
zhang <zhang.com>
2022-08-18 10:45:16 +0800

    threee commit

--- src/main/java/zhang/git/gittest.java
+++ src/main/java/zhang/git/gittest.java
@@ -8,5 +8,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hlloe gits");
     }
 }
