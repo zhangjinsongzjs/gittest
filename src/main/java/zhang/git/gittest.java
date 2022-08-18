9ae1343746ada51c1528be364e875dd173cb2190
zhang <zhang.com>
2022-08-18 10:42:00 +0800

    first commit

---
+++ src/main/java/zhang/git/gittest.java
@@ -0,0 +1,12 @@
+package zhang.git;
+
+/**
+ * @author zhangjinsong
+ * @creat 2022-33-18-10:33
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
