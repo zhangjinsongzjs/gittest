03bf9caf1e0e10833ef4f158b212cebb7de23711
zhang <zhang.com>
2022-08-18 15:03:24 +0800

    gitee mayun

--- src/main/java/zhang/git/gittest.java
+++ src/main/java/zhang/git/gittest.java
@@ -14,5 +14,6 @@ public class gittest {
         System.out.println("hot-fix");
         System.out.println("push");
         System.out.println("pushqwe");
+        System.out.println("gitee");
     }
 }
